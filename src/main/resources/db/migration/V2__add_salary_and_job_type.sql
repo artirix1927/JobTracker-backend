d5c0fe17678bb356e9d58d76daad75269cf5c1fd
Artyom Kotovskiy <artyomkotovskiy@gmail.com>
2026-01-02 00:22:19 -0500

    New Migration Tool
    since the previous one did not support pgsql 18

--- src/main/resources/db/migration/V2__add_salary_and_job_type.sql
+++
@@ -1,8 +0,0 @@
--- =========================
--- JOB POSTS: add salary and job_type
--- =========================
-ALTER TABLE job_post
-ADD COLUMN salary NUMERIC(10,2);
-
-ALTER TABLE job_post
-ADD COLUMN job_type VARCHAR(50);
--- src/main/resources/db/migration/V3__add_location.sql
+++
@@ -1,6 +0,0 @@
--- =========================
--- JOB POSTS: add location field
--- =========================
-
-ALTER TABLE job_post
-ADD COLUMN location VARCHAR(255);
